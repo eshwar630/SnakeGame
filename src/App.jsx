1f4afcc2ffc77835c598e1e7a716d5a1b22b6f97
chinteshwar <chinteshwar345@gmail.com>
2025-06-20 22:27:43 +0530

    thirdchange

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,83 @@
 import './App.css'
-import { useState ,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 function App() {
-  // const grid = Array.from({ length: 400 });
-  const [keyPressed, setKeyPressed] = useState('');
-
+  const grid = Array.from({ length: 400 }, (_, i) => ({ idx: i }));
+  const [head, sethead] = useState(0)
+  const moveRight = () => {
+    let c = head % 20;
+    if (c < 19) {
+      sethead(head + 1)
+    }
+  }
+  const moveLeft = () => {
+    let c = head % 20;
+    if (c > 0) {
+      sethead(head - +1)
+    }
+  }
+  const moveDown = () => {
+    let r = Math.floor(head / 20);
+    if (r < 19) {
+      sethead(head + 20)
+    }
+  }
+  const moveUp = () => {
+    let r = Math.floor(head / 20)
+    if (r > 0) {
+      sethead(head - 20)
+    }
+  }
   useEffect(() => {
     const handleKeyDown = (e) => {
-      setKeyPressed(e.key); // e.key = "ArrowUp", "a", "Enter", etc.
-      console.log('Pressed:', e.key);
+      console.log(e.key)
+      switch (e.key) {
+        case "ArrowUp":
+          moveUp()
+          break;
+        case "ArrowDown":
+          moveDown()
+          break;
+        case "ArrowLeft":
+          moveLeft()
+          break;
+        case "ArrowRight":
+          moveRight()
+          break;
+      }
     };
-
     window.addEventListener('keydown', handleKeyDown);
-
-    // cleanup on unmount
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
   }, []);
   return (
-    <>
+    <> 
+
+      <div className='flex justify-center min-h-screen items-center' key={idx}>
+        <div className='grid-container'>
+          {
+            grid.map((cell)=>{
+              return(
+                <div key={cell.idx} className={head==cell.idx?"head":"cell"}></div>
+              )
+            })
+          }
+        </div>
+      </div>
       {/* <div className='flex justify-center min-h-screen items-center'>
         <div className='grid-container'>
           {grid.map((_, idx) => {
-            return <div key={idx} className='fell'></div>
+            if (idx==0) {
+              return <div key={idx} className='head'></div>
+            }else{
+              return <div key={idx} className='cell'></div>
+            }
           })}
         </div>
       </div> */}
-      <div className="h-screen flex items-center justify-center text-2xl">
+      {/* <div className="h-screen flex items-center justify-center text-2xl">
         Press any key: <span className="ml-2 font-bold">{keyPressed}</span>
-      </div>
+      </div> */}
     </>
   )
 }
