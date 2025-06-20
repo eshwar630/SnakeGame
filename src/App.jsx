a4cfea71d07b82d877f2f60db52de07a3672f3f6
chinteshwar <chinteshwar345@gmail.com>
2025-06-20 21:43:57 +0530

    keyboardinput

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import './App.css'
-import { useState } from 'react'
+import { useState ,useEffect } from 'react'
 function App() {
-  // const [score, setscore] = useState(0)
+  // const grid = Array.from({ length: 400 });
+  const [keyPressed, setKeyPressed] = useState('');
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      setKeyPressed(e.key); // e.key = "ArrowUp", "a", "Enter", etc.
+      console.log('Pressed:', e.key);
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    // cleanup on unmount
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
   return (
     <>
-      <div className='flex justify-center items-center min-h-screen'>
-        <div className="border-2 border-black w-[400px] h-[400px]">
-          <div className='cell w-[20px] h-[20px] bg-blue-500 rounded-lg'></div>
+      {/* <div className='flex justify-center min-h-screen items-center'>
+        <div className='grid-container'>
+          {grid.map((_, idx) => {
+            return <div key={idx} className='fell'></div>
+          })}
         </div>
+      </div> */}
+      <div className="h-screen flex items-center justify-center text-2xl">
+        Press any key: <span className="ml-2 font-bold">{keyPressed}</span>
       </div>
-      
     </>
   )
 }
