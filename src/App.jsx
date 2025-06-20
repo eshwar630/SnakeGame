db3e70a524fcb8f44bc56c4f67bf2404b18828e8
chinteshwar <chinteshwar345@gmail.com>
2025-06-20 18:00:34 +0530

    boxmoved

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { useState } from 'react'
+function App() {
+  // const [score, setscore] = useState(0)
+  return (
+    <>
+      <div className='flex justify-center items-center min-h-screen'>
+        <div className="border-2 border-black w-[400px] h-[400px]">
+          <div className='cell w-[20px] h-[20px] bg-blue-500 rounded-lg'></div>
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
